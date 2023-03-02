126b776eb7cd7a5601445df09fc7b3f673c9de6c
drush92 <dan.rush02@gmail.com>
2023-03-02 17:19:40 +0000

    added vehicles

--- nftropolis/waquest.js
+++ nftropolis/waquest.js
@@ -11,6 +11,12 @@ document.body.appendChild( renderer.domElement );
 var controls = new OrbitControls( camera, renderer.domElement );
 var location;
 
+var tank;
+
+var helicopter;
+
+var carMuscle;
+
 var character;
 var mixer;
 
@@ -41,6 +47,36 @@ loader_location.load( './city/scene.gltf', function ( gltf ) {
     location = gltf.scene;
     location.scale.set(0.005, 0.005, 0.005);
     scene.add( location );
+});
+
+var loader_tank = new GLTFLoader();
+loader_tank.load( './tank/scene.gltf', function ( gltf ) {
+    tank = gltf.scene;
+    scene.add( tank );
+    tank.scale.set(3, 3, 3)
+    tank.position.set(-120, 0, -18);
+    tank.rotateY(1.6);
+    scene.add( tank );
+});
+
+var loader_helicopter = new GLTFLoader();
+loader_helicopter.load( './helicopter/scene.gltf', function ( gltf ) {
+    helicopter = gltf.scene;
+    scene.add( helicopter );
+    helicopter.scale.set(3, 3, 3)
+    helicopter.position.set(-55, 0.5, -18);
+    helicopter.rotateY(0);
+    scene.add( helicopter );
+});
+
+var loader_carMuscle = new GLTFLoader();
+loader_carMuscle.load( './car_muscle/scene.gltf', function ( gltf ) {
+    carMuscle = gltf.scene;
+    scene.add( carMuscle );
+    carMuscle.scale.set(4, 4, 4)
+    carMuscle.position.set(-73, 0, -25);
+    carMuscle.rotateY(0);
+    scene.add( carMuscle );
 });
 
 var loader_character = new GLTFLoader();
@@ -219,10 +255,40 @@ var animate = function () {
     panner_PoliceOfficer.setPosition(camera.position.x, camera.position.y, camera.position.z);
     
     renderer.render( scene, camera );
-    console.log(camera.position);
 
 }
 
 animate();
 
+/*
+NFTropolis is a unique virtual town where users can buy and own NFT characters that have randomized personality traits, 
+which influence the character's dialogue options and role in the overarching background story. These personalities are 
+recorded professionally and updated in patches as the story develops. As users interact with their characters to progress 
+the story, they earn points which can be attributed to skills/personality traits, which in turn affects the story's direction.
+
+One key feature that sets NFTropolis apart is the personalized and dynamic nature of the characters - each NFT character 
+is truly unique, with its own set of traits and dialogue options, meaning that no two users will experience the same story. 
+Additionally, the characters have the potential to influence the course of the story, with their actions having real 
+consequences that can affect the story's outcome.
+
+NFTropolis provides an innovative and engaging way for users to own and interact with NFT characters in a dynamic and 
+ever-changing story. 
+*/
+
+/*
+LOCATION:
+"CCity Building Set 1" (https://skfb.ly/LpSC) by Neberkenezer is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+
+VEHICLES:
+"Transformers Universe: Army Truck" (https://skfb.ly/otyvo) by Primus03 is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+"Transformers Universe: Helicopter" (https://skfb.ly/otyvH) by Primus03 is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+"Transformers Universe: Drive-By's Vehicle Form" (https://skfb.ly/osHDI) by Primus03 is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+
+CHARACTERS:
+"Police" (https://skfb.ly/otoKS) by Alihan is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+"Doctor" (https://skfb.ly/6QZ8M) by 3D-Models is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+"Looking around medical" (https://skfb.ly/ouzT8) by Appsbypaulhamilton is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+"Business Man - Low Polygon game character" (https://skfb.ly/6Ezvq) by manoeldarochadeoliveira is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
+
+*/
 
